6e716803de57224485282ad38e26d42834eeb045
JuanMarulanda4 <79994699+Juan-789@users.noreply.github.com>
2021-11-22 16:40:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+1. Write a program that asks the user to type “true” or “false”. If the user typed true display “The statement is true.” Otherwise display “The statement is false.”
+"""
+
+booleanQuestion = input("type 'true' or 'false '")
+if question.lower == true:
+  print ("The statement is true. ")
+else:
+  print("the statement is false. ")
